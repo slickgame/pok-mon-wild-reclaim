ecf21b75c252e0aa1276795aea2db280eca4238a
slickgame <slickpokefangame@gmail.com>
2026-06-06 15:19:41 -0400

    Add M1.2 explore encounter prototype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import {AppShell} from "@/components/layout/AppShell";
-import {MainMenu} from "@/components/menu/MainMenu";
-import {GameProvider} from "@/game/GameProvider";
-export default function Home(){return <GameProvider><AppShell><MainMenu/></AppShell></GameProvider>;}
+import{useState}from"react";
+import{AppShell}from"@/components/layout/AppShell";
+const party=["Bulbasaur - Support/Sustain","Charmander - Attacker","Squirtle - Tank"];
+const enc=[ ["Caterpie","Lv.2-4","Bug","Scout","Common","A tiny forest Pokemon crawls between the berry stems."], ["Weedle","Lv.2-4","Bug/Poison","Status Specialist","Common","It watches you from a curled leaf."], ["Pidgey
