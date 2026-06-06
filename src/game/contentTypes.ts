8cb480e92c80d3026f092c2fc5f039ae0c9035f2
slickgame <slickpokefangame@gmail.com>
2026-06-06 10:57:30 -0400

    Add M1 content type definitions

---
+++ src/game/contentTypes.ts
@@ -0,0 +1,26 @@
+export type PokemonType =
+  | "Bug"
+  | "Fire"
+  | "Flying"
+  | "Grass"
+  | "Normal"
+  | "Poison"
+  | "Water";
+
+export type CombatRole =
+  | "Attacker"
+  | "Glass Cannon"
+  | "Hazard Seed"
+  | "Scout"
+  | "Stabilizer"
+  | "Status Specialist"
+  | "Support"
+  | "Sustain"
+  | "Tank";
+
+export type MoveCategory = "Physical" | "Special" | "Status";
+
+export type MoveDefinition = {
+  id: string;
+  name: string;
+  type
