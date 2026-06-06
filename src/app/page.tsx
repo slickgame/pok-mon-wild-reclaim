4e23b413a4c5dc1f0f2102770e2561f638e27954
slickgame <slickpokefangame@gmail.com>
2026-06-06 14:56:27 -0400

    Build compact M1.1 playable shell

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 import { AppShell } from "@/components/layout/AppShell";
 import { MainMenu } from "@/components/menu/MainMenu";
-import { GameProvider } from "@/game/GameProvider";
-export default function Home(){return <GameProvider><AppShell><MainMenu /></AppShell></GameProvider>;}
+import { GameProvider, useGame } from "@/game/GameProvider";
+
+const party = [
+  { n: "Bulbasaur", r: "Support / Sustain", hp: "45/45", m: "Tackle, Vine Whip, Growl, Leech Seed", t: "Sprout Guard [C]" },
+  { n: "Charmander", r: "Attacker / Glass Cannon", hp: "39/39", m: "Scratch,
