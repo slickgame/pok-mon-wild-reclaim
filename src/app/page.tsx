e50b01c3980b1fab80dd632fb984c5c897f6750d
slickgame <slickpokefangame@gmail.com>
2026-06-07 16:26:47 -0400

    Use styled M1 shell

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 "use client";
-import {M1DataView} from '@/components/m1/M1DataView';
-export default function Home(){return <main><h1>Pokemon Wild Reclaim</h1><M1DataView/></main>}
+import{M1Shell}from'@/components/m1/M1Shell';
+export default function Home(){return <M1Shell/>}
