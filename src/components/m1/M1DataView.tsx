20b0ebcdc49fd361019ab74190650540d5d956a5
slickgame <slickpokefangame@gmail.com>
2026-06-07 16:13:52 -0400

    Render quest panel

--- src/components/m1/M1DataView.tsx
+++ src/components/m1/M1DataView.tsx
@@ -1,5 +1,6 @@
 import{SettlementPanel}from'./SettlementPanel';
 import{PartyPanel}from'./PartyPanel';
 import{BrambleberryPanel}from'./BrambleberryPanel';
+import{QuestPanel}from'./QuestPanel';
 import{ExplorePanel}from'./ExplorePanel';
-export function M1DataView(){return <section><h2>M1 Vertical Slice</h2><SettlementPanel/><PartyPanel/><BrambleberryPanel/><ExplorePanel/></section>}
+export function M1DataView(){return <section><h2>M1 Vertical Slice</h2><SettlementPanel/><PartyPanel/><QuestPanel/><BrambleberryPanel/><ExplorePanel/></section>}
