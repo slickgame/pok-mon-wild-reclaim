a48f185190d2effe36aa28d0760ead77bdb6708b
slickgame <slickpokefangame@gmail.com>
2026-06-07 16:32:45 -0400

    Improve M1 layout spacing

--- src/components/m1/M1DataView.tsx
+++ src/components/m1/M1DataView.tsx
@@ -1,6 +1,6 @@
 import{SettlementPanel}from'./SettlementPanel';
 import{PartyPanel}from'./PartyPanel';
-import{BrambleberryPanel}from'./BrambleberryPanel';
 import{QuestPanel}from'./QuestPanel';
+import{BrambleberryPanel}from'./BrambleberryPanel';
 import{ExplorePanel}from'./ExplorePanel';
-export function M1DataView(){return <section><h2>M1 Vertical Slice</h2><SettlementPanel/><PartyPanel/><QuestPanel/><BrambleberryPanel/><ExplorePanel/></section>}
+export function M1DataView(){return <section style={{display:'grid',gap:16}}><h2>M1 Vertical Slice</h2><SettlementPanel/><PartyPanel/><QuestPanel/><BrambleberryPanel/><ExplorePanel/></section>}
