3200837161fb47199c5ee1fb893b2d77374611b8
slickgame <slickpokefangame@gmail.com>
2026-06-07 09:31:49 -0400

    Add M1 starter party data

---
+++ src/data/m1/party.ts
@@ -0,0 +1,5 @@
+export const party=[
+ {name:'Bulbasaur',role:'Support / Sustain',hp:'45/45',moves:['Tackle','Vine Whip','Growl','Leech Seed'],talent:'Sprout Guard [C]'},
+ {name:'Charmander',role:'Attacker / Glass Cannon',hp:'39/39',moves:['Scratch','Ember','Growl','Smokescreen'],talent:'Kindled Spirit [C]'},
+ {name:'Squirtle',role:'Tank / Stabilizer',hp:'44/44',moves:['Tackle','Water Gun','Tail Whip','Withdraw'],talent:'Shell Poise [C]'}
+];
